86b4f456ca4b3d483bad74502417a2f8d40fe059
Tim Wahrburg <t.wahrburg@gmail.com>
2024-06-08 14:40:44 +0200

    feat: Hello, Spring Web!

---
+++ src/test/java/org/ooka/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ooka.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
